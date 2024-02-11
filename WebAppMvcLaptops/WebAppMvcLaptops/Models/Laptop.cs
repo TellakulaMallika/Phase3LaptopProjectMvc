d6e1a517d7acf47d79a86bf1224c039b9edf2008
T.Mallika <tellakulamallika@gmailc.om>
2024-02-11 11:32:42 -0700

    firstcommit

---
+++ WebAppMvcLaptops/WebAppMvcLaptops/Models/Laptop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMvcLaptops.Models
+{
+    [Table("Laptop")]
+    public class Laptop
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string RamSize { get; set; }
+        
+    }
+}
